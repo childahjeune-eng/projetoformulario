599c0d7a1cc49bec474f036bc560f1d936a04a56
childah.Al.jeune <childah.jeune@escola.pr.gov.br>
2026-04-24 10:53:17 -0300

    cvb

--- acript.js
+++ acript.js
@@ -1,3 +1,10 @@
 function salvarCarro(event) {
    event.preventDefault();
+
+   let titulo = document.getElementById('title').Value;
+   let preco = Document.getElementById('preco').value;
+   let marca = document.getElementById('marca').value;
+   let modelo = document.getElementById('modelo').value;
+
+   let cambioSelecionado = document.querySelector('input [name="marcha"]:checked');
 }
