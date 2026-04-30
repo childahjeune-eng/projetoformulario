69ed6511f07ef5477f90795569cd28af90d80826
childah.Al.jeune <childah.jeune@escola.pr.gov.br>
2026-04-30 10:16:35 -0300

    cgila

--- acript.js
+++ acript.js
@@ -1,52 +1,52 @@
 function salvarCarro(event) {
-   event.preventDefault();
+    event.preventDefault();
 
-   let titulo = document.getElementById('title').Value;
-   let preco = Document.getElementById('preco').value;
-   let marca = document.getElementById('marca').value;
-   let modelo = document.getElementById('modelo').value;
+    let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value;
 
-   let cambioSelecionado = document.querySelector('input [name="marcha"]:checked');
+    let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
 
-   let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
+    let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
-   let carro = {
-      titulo,
-      preco,
-      marca,
-      modelo,
-      cambio
-   };
-   let carros = JSON.parse(localStorage.getItem("carros")) || [];
-   carros.push(carro);
-   localStorage.setItem("carros", JSON.stringify(carros));
+    let carro = {
+        titulo,
+        preco,
+        marca,
+        modelo,
+        cambio
+    };
 
-   adicionarNaTela(carro);
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+    carros.push(carro);
+    localStorage.setItem("carros", JSON.stringify(carros));
 
-   document.querySelector("form").reset();
+    adicionarNaTela(carro);
+
+    document.querySelector("form").reset();
 }
 
 function adicionarNaTela(carro) {
-   let lista = document.getElementById('listaCarros');
-   let card = document.createElement('div');
-   card.classList.add('card');
+    let lista = document.getElementById('listaCarros');
+    let card = document.createElement('div');
+    card.classList.add('card');
 
-   card.innerHTML = `
-   <h3>${carro.titulo}</h3>
-   <p><strong>preço:</strong> R$ ${carro.preco}</p>
-   <p><strong>marca:</strong> R$ ${carro.marca}</p>
-   <p><strong>modelo:</strong> R$ ${carro.modelo}</p>
-   <p><strong>câmbio:</strong> R$ ${carro.cambio}</p>
-   `
+    card.innerHTML = `
+    <h3>${carro.titulo}</h3>
+    <p><strong>Preço:</strong> R$ ${carro.preco}</p>
+    <p><strong>Marca:</strong>  ${carro.marca}</p>
+    <p><strong>Modelo:</strong>  ${carro.modelo}</p>
+    <p><strong>Câmbio:</strong> ${carro.cambio}</p>
+    `
 
-
-   lista.appendChild(card);
+    lista.appendChild(card);
 }
 
 window.onload = function () {
-   let carros = JSON.parse(localStorage.getItem ('carros')) || [];
+    let carros = JSON.parse(localStorage.getItem('carros')) || [];
 
-   carros,forEach(carro => {
-      adicionarNaTela(carro);
-   });
+    carros.forEach(carro => {
+        adicionarNaTela(carro);
+    });
 }
